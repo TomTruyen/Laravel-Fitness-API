849be45ab9ccbce32a451b9d999db7b5eeb51db6
TomTruyen <tom.truyen@gmail.com>
2022-03-05 15:05:53 +0100

    feat: Exercise

---
+++ app/Models/Exercise.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Exercise extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['name', 'category', 'equipment', 'type'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [];
+}
---
+++ database/seeders/ExerciseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Exercise;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class ExerciseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $exercises = json_decode(File::get("database/data/exercises.json"), true);
+
+        foreach ($exercises as $exercise) {
+            Exercise::firstOrCreate(
+                [
+                    'name' => $exercise['name'],
+                    'category' => $exercise['category'],
+                    'equipment' => $exercise['equipment'],
+                    'type' => $exercise['type'],
+                ],
+            );
+        }
+    }
+}
